034f2f58824bef86034c62f4e18da1164248635b
Nathaniel van Diepen <eeems@omnimaga.org>
2015-02-07 12:53:00 -0700

    allow logging in from the interface

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 window.addEventListener('DOMContentLoaded', function() {
   'use strict';
+  location.hash = 'tab-login';
   navigator.mozL10n.once(function(){
-    
+    window.irc = new IRCCloud();
+    window.$ = function(){
+      return document.querySelector.apply(document,arguments);
+    };
+    $('#login-form').onsubmit = function(){
+      $('#login').click();
+      return false;
+    };
+    $('#login').disabled = false;
+    $('#login').onclick = function(){
+      this.disabled = true;
+      irc.login($('#email').value,$('#password').value,function(d){
+        if(!d.success){
+          $('#login').disabled = false;
+          alert('Login Failed: '+d.message);
+        }else{
+          location.hash = 'tab-display';
+        }
+      });
+    };
   });
 });
